835c33a006a2ea5b840e68766a2d6c5a3715a683
Jakub Perlin <jakubperlin@gmail.com>
2018-02-08 21:13:30 +0000

    Racket model changes, started setting up 1st training scene
    - Changed the racket models in Blender so that the origins of the models are in the centre of the racket, not at the tip.
    - Created a new scene. Its purpose is the 1st attempt at using ML.
    - Set up the ball Shooter in the new scene.
    - Starting setting up the ML Unity code. Left some //TODO comments in the PPAgent.cs.

---
+++ VR Ping Pong AI/Assets/Scripts/ML/PPAgent.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PPAgent : Agent
+{
+    [Header("Specific to Ping Pong")]
+	public Rigidbody ball;
+    public bool invertXZ;
+
+	private float invertXZMult;
+	private Vector3 defaultRacketPos;
+	private Quaternion defaultRacketRot;
+
+	void Start()
+	{
+		invertXZMult = invertXZ ? -1 : 1;
+		defaultRacketPos = gameObject.transform.position;
+		defaultRacketRot = gameObject.transform.rotation;
+	}
+
+    public override List<float> CollectState()
+    {
+        List<float> state = new List<float>();
+
+		// Racket pos:
+        state.Add(invertXZMult * gameObject.transform.position.x);
+		state.Add(               gameObject.transform.position.y);
+		state.Add(invertXZMult * gameObject.transform.position.z);
+
+		// Racket rot:
+		state.Add(invertXZMult * gameObject.transform.rotation.eulerAngles.x);
+		state.Add(               gameObject.transform.rotation.eulerAngles.y);
+		state.Add(invertXZMult * gameObject.transform.rotation.eulerAngles.z);
+
+		// Ball pos:
+		state.Add(invertXZMult * ball.transform.position.x);
+		state.Add(               ball.transform.position.y);
+		state.Add(invertXZMult * ball.transform.position.z);
+
+		// Ball vel:
+		state.Add(invertXZMult * ball.velocity.x);
+		state.Add(               ball.velocity.y);
+		state.Add(invertXZMult * ball.velocity.z);
+
+        return state;
+    }
+
+    // to be implemented by the developer
+    public override void AgentStep(float[] act)
+    {
+		Vector3 requestedPos = new Vector3(act[0], act[1], act[2]);
+		Vector3 requestedRot = new Vector3(act[3], act[4], act[5]);
+
+		// TODO: 
+		// Make public variables maxSpeed and maxAngularSpeed so that you can set them from editor.
+		// Set new position based on the requests as well as maxSpeed and maxAngularSpeed.
+
+		// Hmm... Perhaps we'll have to be smart here and measure time since the last update or something.
+		// Because to use the max velocity limit, we need to know over what time the movement takes place.
+		// If there's gonna be a lot of time between the actions, then we'll have to smooth out the motion of the bat,
+		// not just have it jump to the new position.
+
+		// So perhaps for now ignore the maxSpeed and maxAngSpeed limits and just teleport the bat! We'll see how this turns out :)
+    }
+
+    // to be implemented by the developer
+    public override void AgentReset()
+    {
+		// TODO:
+		// Figure out where this is called from and when.
+		// Modify this method if necessary.
+
+		gameObject.transform.position = defaultRacketPos;
+		gameObject.transform.rotation = defaultRacketRot;
+    }
+}
---
+++ VR Ping Pong AI/Assets/Scripts/Shooter.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
+public class Shooter : MonoBehaviour {
+
+	private const float	REWARD_WIN  =  1.0f, // Given when the racket returns the ball successfully or the shooter misses and the racket doesn't hit the ball.
+						REWARD_HIT  =  0.1f, // Given when the racket hit the ball in the right state, but the ball didn't go in.
+						REWARD_FAIL = -1.0f; // Given when the racket loses a point, but is not eligible for the HIT reward.
+
+	public Agent trainee;
+	public Collider	floor, 
+					closerTable, 			// The half closer to racket.
+					furtherTable, 			// The half further from racket.
+					racket;					// The racket of the bot being trained.
+
+	private enum eShooterState{ State0, State1, State2 };
+	private enum eShooterTransition{ ST_RACK, ST_BADTABLE, ST_GOODTABLE, ST_FLOOR };
+	private eShooterState currState = eShooterState.State0;
+
+	private Vector3 defaultBallPos;
+	private Rigidbody ballBody;
+
+	private float invertXZMult;
+
+	void Start()
+	{
+		defaultBallPos = gameObject.transform.position;
+		ballBody = gameObject.GetComponent<Rigidbody>();
+		invertXZMult = trainee.gameObject.GetComponent<PPAgent>().invertXZ ? -1f : 1f;
+
+		currState = eShooterState.State0;
+		ShootBall();
+	}
+
+	void RewardAndReset(float reward)
+	{
+		trainee.reward = reward;
+		ShootBall();
+	}
+
+	void MakeStep(eShooterTransition transition)
+	{
+		switch(currState)
+		{
+		case eShooterState.State0:
+			switch (transition)
+			{
+			case eShooterTransition.ST_BADTABLE:
+				currState = eShooterState.State1;
+				break;
+			case eShooterTransition.ST_RACK:
+				RewardAndReset(REWARD_FAIL);
+				break;
+			default:
+				RewardAndReset(REWARD_WIN);
+				break;
+			}
+			break;
+		case eShooterState.State1:
+			switch (transition)
+			{
+			case eShooterTransition.ST_RACK:
+				currState = eShooterState.State2;
+				break;
+			default:
+				RewardAndReset(REWARD_FAIL);
+				break;
+			}
+			break;
+		case eShooterState.State2:
+			switch (transition)
+			{
+			case eShooterTransition.ST_GOODTABLE:
+				RewardAndReset(REWARD_WIN);
+				break;
+			default:
+				RewardAndReset(REWARD_HIT);
+				break;
+			}
+			break;
+		}
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		Debug.Log("Collided with " + col.gameObject.name);
+		if (col.gameObject == floor.gameObject)
+		{
+			MakeStep(eShooterTransition.ST_FLOOR);
+		} 
+		else if (col.gameObject == closerTable.gameObject)
+		{
+			MakeStep(eShooterTransition.ST_BADTABLE);
+		}
+		else if (col.gameObject == furtherTable.gameObject)
+		{
+			MakeStep(eShooterTransition.ST_GOODTABLE);
+		}
+		else if (col.gameObject == racket.gameObject)
+		{
+			MakeStep(eShooterTransition.ST_RACK);
+		}
+	}
+
+
+
+
+	public float minX, maxX, minZ, maxZ;
+
+	void ShootBall()
+	{
+		currState = eShooterState.State0;
+		gameObject.transform.position = defaultBallPos;
+		float x = Random.Range(minX, maxX);
+		float z = Random.Range(minZ, maxZ);
+		ballBody.velocity = Vector3.zero;
+		ballBody.AddForce(new Vector3(x, 0, z) * invertXZMult, ForceMode.Impulse);
+	}
+
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			currState = eShooterState.State0;
+			ShootBall();
+		}
+	}
+}
