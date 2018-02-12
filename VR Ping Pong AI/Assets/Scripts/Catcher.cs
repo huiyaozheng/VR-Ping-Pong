0967a62abb5b5aef1aea33cd6c1d507f6cb9f7dd
huiyaozheng(Alex) <huiyaozheng@gmail.com>
2018-02-12 00:27:27 +0000

    Tracking the ball's x position

--- VR Ping Pong AI/Assets/Scripts/Catcher.cs
+++ VR Ping Pong AI/Assets/Scripts/Catcher.cs
@@ -1,22 +1,45 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Catcher : MonoBehaviour
 {
     public GameState game;
+    public Rigidbody ball;
 
-    private Rigidbody ball;
-    private Rigidbody racket;
+    /// <summary>
+    /// The racket controlled by this script.
+    /// </summary>
+    public Rigidbody myRacket;
 
-    private Collider closerTable;
+    /// <summary>
+    /// The opponent's racket.
+    /// </summary>
+    public Rigidbody opponentRacket;
+
+    /// <summary>
+    /// This side of the table.
+    /// </summary>
+    public Collider closerTable;
+
+    /// <summary>
+    /// Opponent's side of table.
+    /// </summary>
+    public Collider opponentTable;
+
+    /// <summary>
+    /// Z-axis distance between the ball and myRacket. Used to detect whether the ball is flying towards us.
+    /// </summary>
+    private float prevZDistance;
 
     /// <summary>
     /// Predict where the ball will hit the closer table.
     /// </summary>
-    void predict()
+    public void predict()
     {
-        
+        //TODO: predict the highest point in the ball's trajectory and move the racket there.
     }
 
     /// <summary>
@@ -24,7 +47,6 @@ public class Catcher : MonoBehaviour
     /// </summary>
     void move()
     {
-        
     }
 
     /// <summary>
@@ -41,18 +63,32 @@ public class Catcher : MonoBehaviour
     void hit()
     {
         Vector3 target = setTarget();
+        ball.AddForce(new Vector3(0, 0, -10), ForceMode.Impulse);
     }
 
-	// Use this for initialization
-	void Start ()
-	{
-	    ball = game.ball;
-	    closerTable = game.table0;
-	    racket = game.racket0;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        prevZDistance = Math.Abs(ball.transform.position.z - myRacket.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentZDistance = Math.Abs(ball.transform.position.z - myRacket.transform.position.z);
+        if (currentZDistance < prevZDistance)
+        {
+            myRacket.transform.position = new Vector3(ball.transform.position.x, myRacket.transform.position.y,
+                myRacket.transform.position.z);
+        }
+        prevZDistance = currentZDistance;
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject == ball.gameObject)
+        {
+            hit();
+        }
+    }
 }
