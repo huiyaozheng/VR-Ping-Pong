23689a002c725151393a216bc27585da2a77a979
Jakub Perlin <jakubperlin@gmail.com>
2018-02-18 19:37:47 +0000

    Added ball sounds
    The volume depends on the speed of the collision. We can modify the pitch in the same way if that'd sound good.

---
+++ VR Ping Pong AI/Assets/Scripts/GamePhysics/BallAudio.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
+public class BallAudio : MonoBehaviour {
+
+	public AudioSource audioSource;
+	public float minVolume;
+	public float maxVolume;
+	public float minVolumeSpeed;
+	public float maxVolumeSpeed;
+
+	void OnCollisionEnter(Collision col)
+	{
+		Vector3 ballVel = gameObject.GetComponent<Rigidbody>().velocity;
+		Vector3 otherVel = Vector3.zero;
+
+		Rigidbody otherBody = col.gameObject.GetComponent<Rigidbody>();
+		if (otherBody != null)
+		{
+			otherVel = otherBody.velocity;
+		}
+
+		float loudnessFactor = (ballVel - otherVel).magnitude;
+		Debug.Log(loudnessFactor);
+		loudnessFactor = Mathf.Clamp(loudnessFactor, minVolumeSpeed, maxVolumeSpeed);
+		float volume = (maxVolume - minVolume) * (loudnessFactor - minVolumeSpeed) / (maxVolumeSpeed - minVolumeSpeed) + minVolume;
+
+		audioSource.volume = volume;
+		audioSource.Play();
+	}
+}
