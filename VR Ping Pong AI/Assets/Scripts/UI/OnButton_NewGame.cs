b8f503c92ac8f547023099b738b288709f8fae57
Jakub Perlin <jakubperlin@gmail.com>
2018-02-27 16:06:59 +0000

    Added RallyState and started looking at UI

---
+++ VR Ping Pong AI/Assets/Scripts/UI/OnButton_NewGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnButton_NewGame : MonoBehaviour {
+
+	public List<GameObject> objectsToSetInactive, objectsToSetActive;
+
+	public void OnClick()
+	{
+		foreach (GameObject go in objectsToSetActive)
+			go.SetActive(true);
+		foreach (GameObject go in objectsToSetInactive)
+			go.SetActive(false);
+	}
+}
