b79d1096c2caba33b8ac4630aadcc6e45ff65f7b
Jakub Perlin <jakubperlin@gmail.com>
2018-02-28 17:40:49 +0000

    Added Sam's scripts, but not scene change
    Co-Authored-By: sjg206 <sjg206@users.noreply.github.com>

---
+++ VR Ping Pong AI/Assets/Scripts/Settings/HeightSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeightSettings : MonoBehaviour {
+
+    private static int Height;
+
+    public static int GetHeight()
+    {
+        return Height;
+    }
+
+    public static void SetHeight(int h)
+    {
+        Height = h;
+    }
+}
