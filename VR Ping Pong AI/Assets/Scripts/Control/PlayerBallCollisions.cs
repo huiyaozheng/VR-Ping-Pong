82537766dfe8093c739f882ae8a0071b46df6e73
Jakub Perlin <jakubperlin@gmail.com>
2018-03-05 02:28:40 +0000

    An idea for player-ball collisions
    Based on tracking racket's positions throughout frames.
    
    Co-Authored-By: sjg206 <sjg206@users.noreply.github.com>

--- VR Ping Pong AI/Assets/Scripts/Control/PlayerBallCollisions.cs
+++ VR Ping Pong AI/Assets/Scripts/Control/PlayerBallCollisions.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 
 public class PlayerBallCollisions : MonoBehaviour {
 
+	public List<Vector3> racketPositions = new List<Vector3>();
+	public int positionTrackingDepthInFrames = 10; // 5 or 10 seems reasonable but not completely sure!
+	public float speedMultiplier = 10f; // no idea what value will be good here, experiment needed
+
+
+	void Update()
+	{
+		racketPositions.Insert(0, transform.position);
+		if (racketPositions.Count > positionTrackingDepthInFrames)
+			racketPositions.RemoveAt(positionTrackingDepthInFrames);
+	}
+
 	public GameObject ball;
 	public Rigidbody myRacketBody;
 
 	void OnCollisionEnter(Collision col)
 	{
+		int index = Mathf.Min(racketPositions.Count - 1, positionTrackingDepthInFrames - 1);
+		if (index < 0)
+			return;
+		
+		Vector3 racketVelocity = (transform.position - racketPositions[index]) / (float)positionTrackingDepthInFrames; // normalize by dividing by the number of frames in between
+		racketVelocity *= speedMultiplier;
+
 		if (col.gameObject == ball)
 		{
-			Debug.Log("AWESOME COLLISION PHYSICS ENABLED NOW");
+			Debug.Log("AWESOME COLLISION PHYSICS ACTING NOW");
 			Rigidbody ballBody = ball.GetComponent<Rigidbody>();
-			ballBody.velocity = myRacketBody.velocity * 1.2f;
+			ballBody.velocity = racketVelocity;
 		}
 	}
 }
