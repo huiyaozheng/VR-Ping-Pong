c0ab9cf5a3bb62698be5d11c1b309a2f087fd6da
Jakub Perlin <jakubperlin@gmail.com>
2018-03-05 02:46:43 +0000

    Take the normal into account in the player-ball collision.
    Co-Authored-By: sjg206 <sjg206@users.noreply.github.com>

--- VR Ping Pong AI/Assets/Scripts/Control/PlayerBallCollisions.cs
+++ VR Ping Pong AI/Assets/Scripts/Control/PlayerBallCollisions.cs
@@ -6,14 +6,16 @@ public class PlayerBallCollisions : MonoBehaviour {
 
 	public List<Vector3> racketPositions = new List<Vector3>();
 	public int positionTrackingDepthInFrames = 10; // 5 or 10 seems reasonable but not completely sure!
-	public float speedMultiplier = 10f; // no idea what value will be good here, experiment needed
-
+	public float speedMultiplier = 340f; // no idea what value will be good here, experiment needed
+	public float normalInfluence = 0.1f; // We multiply the (unit) normal vector by the length of the racket velocity, and then by this number!
 
 	void Update()
 	{
 		racketPositions.Insert(0, transform.position);
 		if (racketPositions.Count > positionTrackingDepthInFrames)
 			racketPositions.RemoveAt(positionTrackingDepthInFrames);
+
+		Debug.Log(transform.forward);
 	}
 
 	public GameObject ball;
@@ -28,11 +30,20 @@ public class PlayerBallCollisions : MonoBehaviour {
 		Vector3 racketVelocity = (transform.position - racketPositions[index]) / (float)positionTrackingDepthInFrames; // normalize by dividing by the number of frames in between
 		racketVelocity *= speedMultiplier;
 
+
 		if (col.gameObject == ball)
 		{
 			Debug.Log("AWESOME COLLISION PHYSICS ACTING NOW");
+
 			Rigidbody ballBody = ball.GetComponent<Rigidbody>();
-			ballBody.velocity = racketVelocity;
+
+			Vector3 racketNormal = transform.forward;
+			if (Vector3.Dot(racketVelocity, racketNormal) < 0) // We want the normal to face the same way as racket velocity, rather than opposite
+				racketNormal *= -1;
+
+			racketNormal = racketNormal.normalized;
+
+			ballBody.velocity = racketVelocity + racketNormal * racketVelocity.magnitude * normalInfluence;
 		}
 	}
 }
